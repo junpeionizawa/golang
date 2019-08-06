eecf4c0b110c8e8cbc650e5adb54cb452f72c89b
junpei onizawa <onizawajunpei4@gmail.com>
2019-08-06 23:41:31 +0900

    曜日と月の日本語変換

--- src/github.com/junpeionizawa/callender/hello.go
+++ src/github.com/junpeionizawa/callender/hello.go
@@ -8,13 +8,17 @@ import "time"
 
 func main() {
 	//現在の時間を代入、:=は関数内のみ使える
+
+	//曜日、月を日本語変換
+	week := [...]string{"日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"}
+	month := [...]string{"","1月","2月","3月","4月","５月","6月","7月","8月","9月","10月","11月","12月"}
     t := time.Now()
     fmt.Println(t)           
     fmt.Println(t.Year())   
-    fmt.Println(t.Month())   
+    fmt.Println(month[t.Month()])   
     fmt.Println(t.Day())   
+    fmt.Println(week[t.Weekday()]) 
     fmt.Println(t.Hour())    
     fmt.Println(t.Minute())  
     fmt.Println(t.Second())  
-    fmt.Println(t.Weekday()) 
 }
