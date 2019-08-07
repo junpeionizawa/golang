793107a9e230de6f9151e98ee8e2d4b951b79c51
junpei onizawa <onizawajunpei4@gmail.com>
2019-08-07 21:47:49 +0900

    フォルダ、html,cssファイル追加

--- src/github.com/junpeionizawa/callender/hello.go
+++ src/github.com/junpeionizawa/callender/hello.go
@@ -2,42 +2,18 @@
 
 package main
 
-//import "fmt"
-//timeモジュールの機能を使う
-import "time"
-import "html/template"
-import "os"
-import "log"
+import (
+    "log"
+    "net/http"
+)
+
 func main() {
-	//現在の時間を代入、:=は関数内のみ使える
+    //ディレクトリを指定する
+    fs := http.FileServer(http.Dir("static"))
+    //ルーティング設定。"/"というアクセスがきたらstaticディレクトリのコンテンツを表示させる
+    http.Handle("/", fs)
 
-	//曜日、月を日本語変換
-	//week := [7]string{"日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"}
-	//month := [13]string{"","1月","2月","3月","4月","５月","6月","7月","8月","9月","10月","11月","12月"}
-    //t := time.Now()
-    //a := "/"
-    //b := "日"
-    //t1 := t.AddDate(0,0,-1 * t.Day())
-    //t2 := t.AddDate(0,0,-1 * t.Day() + 1)
-    //t3 := t.AddDate(0,1,-1 * t.Day())
-
-    //fmt.Println(t1)   
-    //fmt.Println(t2)
-    //fmt.Println(t3)
-    //fmt.Println(t.Add(24 * time.Hour))
-
-    //fmt.Println(t.Year(),a,month[t.Month()],t.Day(),a,week[t.Weekday()])   
-    //fmt.Println(month[t.Month()])   
-    //fmt.Println(t.Day(),b)  
-    //fmt.Println(week[t.Weekday()]) 
-    //mt.Println(t.Hour())    
-    //fmt.Println(t.Minute())  
-    //fmt.Println(t.Second())  
-    tt := template.Must(template.ParseFiles("./index.html"))
-
-	// 値の埋め込み
-	t := time.Now()
-	if err := tt.Execute(os.Stdout, t); err != nil {
-		log.Fatal(err)
-	}
+    log.Println("Listening...")
+    // 3000ポートでサーバーを立ち上げる
+    http.ListenAndServe(":3000", nil)
 }
