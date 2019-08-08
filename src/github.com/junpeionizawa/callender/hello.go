89d8970444a8bebfd5def390283d0c038ab50742
junpei onizawa <onizawajunpei4@gmail.com>
2019-08-09 00:49:19 +0900

    カレンダーの外枠設計（仮）

--- src/github.com/junpeionizawa/callender/hello.go
+++ src/github.com/junpeionizawa/callender/hello.go
@@ -3,17 +3,25 @@
 package main
 
 import (
+	//"bytes"
+	//"fmt"
+	//"io"
+    //"os"
+    //"html/template"
     "log"
     "net/http"
+    //"time"
 )
 
 func main() {
     //ディレクトリを指定する
-    fs := http.FileServer(http.Dir("static"))
+    fs := http.FileServer(http.Dir("layout"))
     //ルーティング設定。"/"というアクセスがきたらstaticディレクトリのコンテンツを表示させる
     http.Handle("/", fs)
 
     log.Println("Listening...")
     // 3000ポートでサーバーを立ち上げる
     http.ListenAndServe(":3000", nil)
+
+   
 }
