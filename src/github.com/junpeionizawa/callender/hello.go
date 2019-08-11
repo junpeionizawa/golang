1ff857a4f05d7694a13adc57bae14f3f1803d08e
junpei onizawa <onizawajunpei4@gmail.com>
2019-08-11 23:50:35 +0900

    提出物１

--- src/github.com/junpeionizawa/callender/hello.go
+++ src/github.com/junpeionizawa/callender/hello.go
@@ -10,13 +10,6 @@ import (
 )
 func main() {
     fmt.Println("The Server runs with http://localhost:3000/")
-    // ここで静的ファイルであるCSSを適用
-    // Handler : 第一引数で与えたパターンに対して、第二引数Handlerを登録
-    // StripPrefix : 第一引数で与えたURLのパスを取り除いて、第二引数Handlerを発動させる
-    // FileServer : 、HTTPリクエストに対して、第一引数のrootを起点とするファイルシステムのコンテンツを返すハンドラを返す
-    // ここでいうrootは、resources
-
-    // つまり、resourcesディレクトリ以下の静的ファイル(ここでいうcss/index.css)を探して返す
     http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources/"))))
     http.HandleFunc("/", Handler)
     http.ListenAndServe(":3000", nil)
@@ -24,47 +17,30 @@ func main() {
 
 type Time struct {
     Year int
-    Month int
+    Month string
     Day1 int
-    Day2 interface{}
-    Day3 interface{}
-    Day4 interface{}
-    Day5 interface{}
-    Day6 interface{}
-    Day7 interface{}
     Weekday string
-    Weekday2 string
-    Weekday3 string
-    Weekday4 string
-    Weekday5 string
-    Weekday6 string
-    Weekday7 string
+    Hour int
+    Minute int
+    Second int
+   
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
     t := Time{
         Year:time.Now().Year(),
-        Month:int(time.Now().Month()),
-        Day1:time.Now().Day(),
-        Day2:time.Now().Add(24 * time.Hour).Day(),
-        Day3 :time.Now().Add(48 * time.Hour).Day(),
-        Day4 :time.Now().Add(72 * time.Hour).Day(),
-        Day5 :time.Now().Add(96 * time.Hour).Day(),
-        Day6 :time.Now().Add(120 * time.Hour).Day(),
-        Day7 :time.Now().Add(144 * time.Hour).Day(),
+        Month:time.Now().Month().String(),        
+        Day1 :time.Now().Day(),
+        Hour :time.Now().Hour(),
+        Minute:time.Now().Minute(),
+        Second:time.Now().Second(),
         Weekday:time.Now().Weekday().String(),
-        Weekday2:time.Now().Add(24 * time.Hour).Weekday().String(),
-        Weekday3:time.Now().Add(48 * time.Hour).Weekday().String(),
-        Weekday4:time.Now().Add(72 * time.Hour).Weekday().String(),
-        Weekday5:time.Now().Add(96 * time.Hour).Weekday().String(),
-        Weekday6:time.Now().Add(120 * time.Hour).Weekday().String(),
-        Weekday7:time.Now().Add(144 * time.Hour).Weekday().String(),
     }
 
 
 
-    tmpl := template.Must(template.ParseFiles("./resources/index.html"))
+    tmpl := template.Must(template.ParseFiles("./resources/index.php"))
     tmpl.Execute(w, t)
 
 } 
